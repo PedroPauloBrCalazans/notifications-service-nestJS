9d3e606905c036a6a7c4eb704ce11294d3267d60
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-01-19 20:09:31 -0300

    feat: configurando o jest + add test de content e notification.

---
+++ src/application/entities/content.spec.ts
@@ -0,0 +1,17 @@
+import { Content } from './content';
+
+describe('Testes relacionados ao Conteúdo da notificação.', () => {
+  it('Deve ser possível criar o conteúdo da notificação', () => {
+    const content = new Content('Você recebeu uma solicitação de amizade');
+
+    expect(content).toBeTruthy();
+  });
+
+  it('Não deve ser possível criar o conteúdo da notificação com menos de 5 caracteres', () => {
+    expect(() => new Content('vo')).toThrow(); //quando eu criar um conteúdo com menos de 5 deve disparar um erro
+  });
+
+  it('Não deve ser possível criar o conteúdo da notificação com mais de 240 caracteres', () => {
+    expect(() => new Content('vo'.repeat(242))).toThrow();
+  });
+});
---
+++ src/application/entities/notification.spec.ts
@@ -0,0 +1,14 @@
+import { Content } from './content';
+import { Notification } from './notification';
+
+describe('Entidade de Notificação', () => {
+  it('Deve ser possível criar Notificação', () => {
+    const notification = new Notification({
+      content: new Content('Nova solicitação'),
+      category: 'social',
+      recipientId: 'exemplo-recipient-id',
+    });
+
+    expect(notification).toBeTruthy();
+  });
+});
