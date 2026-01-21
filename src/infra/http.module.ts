575662da61e59189ccec1c677532b7510ea8c16c
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-01-21 01:41:34 -0300

    feat: Add repositório do Prisma + conectando à camada de infraestrutura + Ajustes de pastas e arquivos.

---
+++ src/infra/http.module.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { NotificationsController } from './http/controller/notifications.controller';
+import { SendNotification } from 'src/application/use-cases/send-notification';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [NotificationsController],
+  providers: [SendNotification] //pra conseguir usar o send dentro do controller preciso colocar dentro do provider, se não, não conseguimos fazer injeção de depen..
+})
+export class HttpModule {}
