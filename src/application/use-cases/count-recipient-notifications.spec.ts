89492367f280b446e417ab8a10450869439c5aa3
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-01-24 00:10:23 -0300

    feat + test: Implementação da contagem de notificações por recipient + test

---
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,44 @@
+/* eslint-disable prettier/prettier */
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/content';
+import { CountRecipientNotifications } from './count-recipient-notifications';
+
+describe('Count notificação', () => {
+  it('Count notificação', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotifications = new CountRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'Social',
+        content: new Content('Nova solicitação de amizade!'),
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'Social',
+        content: new Content('Nova solicitação de amizade!'),
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'Social',
+        content: new Content('Nova solicitação de amizade!'),
+        recipientId: 'recipient-2',
+      }),
+    );
+
+    const { count } = await countRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(count).toEqual(2);
+  });
+});
