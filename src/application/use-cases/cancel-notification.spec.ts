83682f9c005f9b9994a2fbbd4e5c3bc58dc37d51
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-01-23 21:18:03 -0300

    test: Add teste pra não ser possível cancelar um notificação inexistente.

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -1,7 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
 import { Notification } from '@application/entities/notification';
 import { Content } from '@application/entities/content';
+import { NotificationNotFound } from './errors/notification-not-found';
 
 describe('Cancelar notificação', () => {
   it('Deve cancelar a notificação', async () => {
@@ -23,5 +25,16 @@ describe('Cancelar notificação', () => {
     expect(notificationsRepository.notifications[0].canceledAt).toEqual(
       expect.any(Date),
     );
+  });
+
+  it('Não deveria ser possível cancelar uma notificação inexistente.', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
   });
 });
