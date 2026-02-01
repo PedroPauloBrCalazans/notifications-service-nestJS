6215707c12e5f524f65b1265f0ce1fa51c6ed44c
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-02-01 01:20:42 -0300

    feat: add eslint-disable

--- src/infra/http/controller/notifications.controller.ts
+++ src/infra/http/controller/notifications.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { SendNotification } from '@application/use-cases/send-notification';
