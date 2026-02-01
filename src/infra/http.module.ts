446105954c2b821c5a6f25bb3f5532eace3a5a62
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-02-01 01:12:29 -0300

    feat: Add Rotas HTTP.

--- src/infra/http.module.ts
+++ src/infra/http.module.ts
@@ -3,10 +3,23 @@ import { Module } from '@nestjs/common';
 import { NotificationsController } from './http/controller/notifications.controller';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from './database/database.module';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification] //pra conseguir usar o send dentro do controller preciso colocar dentro do provider, se não, não conseguimos fazer injeção de depen..
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ], //pra conseguir usar o send dentro do controller preciso colocar dentro do provider, se não,
+  //  não conseguimos fazer injeção de depen..
 })
 export class HttpModule {}
