aae2c7107098367c9c18bc4e34396845584627ba
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-02-15 00:58:01 -0300

    feat: Add controller no kafka para notificações + Ajustes.

--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,10 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { KafkaConsumerService } from './kafka/kafka-consumer.service';
+import { NotificationsController } from './kafka/controllers/notifications.controller';
+import { SendNotification } from '@application/use-cases/send-notification';
+import { DatabaseModule } from '@infra/database/database.module';
 
 @Module({
-  imports: [],
-  providers: [KafkaConsumerService],
-  controllers: [],
+  imports: [DatabaseModule],
+  providers: [KafkaConsumerService, SendNotification],
+  controllers: [NotificationsController],
 })
 export class MessagingModule {}
